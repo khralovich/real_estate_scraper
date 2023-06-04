442708cf31832dfcac4be80f192c11da43687019
khralovich <khralovich@protonmail.com>
2023-06-04 22:13:47 +0200

    add comments

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Strona:
+    chromedriver_path = None
+
+    def __init__(self, path, path_query):
+        self.path = path
+        self.path_query = path_query
+    # here define methods
+    # def()
--- index.py
+++ index.py
@@ -5,39 +5,74 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
-path = "/usr/bin/chromedriver"
+chromedriver_path = "/usr/bin/chromedriver"
 domain = "https://www.morizon.pl"
 morizon_main_url = "https://www.morizon.pl/mieszkania/warszawa/?ps%5Bbuild_year_from%5D=1970&ps%5Bliving_area_from%5D=40&ps%5Bmarket_type%5D=2&ps%5Bnumber_of_rooms_from%5D=2&ps%5Bnumber_of_rooms_to%5D=3&ps%5Bprice_m2_to%5D=10979&ps%5Bprice_to%5D=500000"
 
+"""
+Create a general class for scraping a page.
+Returns: a soup object (an entire html)
+"""
+
 
 def get_html(url):
-    driver = webdriver.Chrome(path)
+    driver = webdriver.Chrome(chromedriver_path)
     driver.get(url)
     time.sleep(5)
     html = driver.page_source
     time.sleep(10)
-    return bs(html, "html.parser")
+    soup = bs(html, "html.parser")
+    return soup
 
 
-def get_ads_urls_set(url_search):
-    main_page_html = get_html(url_search)
+"""
+Get urls of all ads we would like to scrape.
+Arguments to pass: a path with a query (filters)
+Returns: a set of links with ads of interest.
+"""
+
+
+def get_ads_urls_set(path_with_filters):
+    main_page_html = get_html(path_with_filters)
     all_offers_html = main_page_html.find_all("a", {"class": "offer__outer"})
-    offers_set = set()
+    offers_paths_set = set()
     for i in all_offers_html:
         link = i['href']
         full_url = domain + link
-        offers_set.add(full_url)
-    return offers_set
+        offers_paths_set.add(full_url)
+    print(offers_paths_set)
+    return offers_paths_set
 
 
-def get_single_offer(ad_url):
-    html_adv = get_html(ad_url)
+"""
+A function for a single ad. Should be in a loop.
+"""
 
 
-offers_set = get_ads_urls_set(morizon_main_url)
+def get_single_offer(offer):
+    adv_html = get_html(offer)
+    product_card = adv_html.find_all("div", {"class": "basic-info"})
+    # do smth to get all needed data
+    # put it into a df
+    # get next ad
 
 
-print(offers_set)
+"""
+Loop through ads of interest, scrape each one of them.
+Arguments to pass: a set of urls.
+Returns: a df with info for each ad.
+"""
+
+
+def get_data_for_regex(offers_paths_set):
+    ads_df = pd.DataFrame()
+    for i in offers_paths_set:
+        single_offer_row = get_single_offer(i)
+        ads_df = ads_df.append(single_offer_row, ignore_index=True)
+    return ads_df
+
+
+offers_paths_set = get_ads_urls_set(morizon_main_url)
 
 
 """
