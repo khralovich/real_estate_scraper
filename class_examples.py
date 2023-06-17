890a1a584c422194d2b3bd60bd0acfec539a1e45
khralovich <khralovich@protonmail.com>
2023-06-17 23:20:34 +0200

    add classes examples

---
+++ class_examples.py
@@ -0,0 +1,112 @@
+
+"""
+EXAMPLE OF CLASS-LEVEL ATTRIBUTES
+class Player:
+    MAX_POSITION = 10
+    def __init__(self):
+        self.position = 0
+
+    # Add a move() method with steps parameter
+    def move(self, steps):
+        self.steps = steps
+        if self.position + self.steps < Player.MAX_POSITION:
+            self.position = self.position + self.steps
+        else:
+            self.position = Player.MAX_POSITION
+        return self.position
+
+    # This method provides a rudimentary visualization in the console
+    def draw(self):
+        drawing = "-" * self.position + "|" +"-"*(Player.MAX_POSITION - self.position)
+        print(drawing)
+
+p = Player(); p.draw()
+p.move(4); p.draw()
+p.move(5); p.draw()
+p.move(3); p.draw()
+"""
+
+
+"""
+EXAMPLE OF CLASSMETHODS
+
+# import datetime from datetime
+from datetime import datetime
+
+class BetterDate:
+    def __init__(self, year, month, day):
+      self.year, self.month, self.day = year, month, day
+
+    @classmethod
+    def from_str(cls, datestr):
+        year, month, day = map(int, datestr.split("-"))
+        return cls(year, month, day)
+
+    # Define a class method from_datetime accepting a datetime object
+    @classmethod
+    def from_datetime(cls, datetimeobj):
+      day = datetimeobj.day
+      month = datetimeobj.month
+      year = datetimeobj.year
+      return  cls(year, month, day)
+
+# You should be able to run the code below with no errors:
+today = datetime.today()
+bd = BetterDate.from_datetime(today)
+print(bd.year)
+print(bd.month)
+print(bd.day)
+"""
+
+
+"""
+CLASS INHERIT ANOTHER CLASS
+
+class Employee:
+  MIN_SALARY = 30000
+
+  def __init__(self, name, salary=MIN_SALARY):
+      self.name = name
+      if salary >= Employee.MIN_SALARY:
+        self.salary = salary
+      else:
+        self.salary = Employee.MIN_SALARY
+  def give_raise(self, amount):
+    self.salary += amount      
+        
+# MODIFY Manager class and add a display method
+class Manager(Employee):
+  def display(self):
+    print("Manager" + self.name)
+
+mng = Manager("Debbie Lashko", 86500)
+print(mng.name)
+
+# Call mng.display()
+mng.display()
+"""
+
+"""
+METHOD INHERITANCE AND POLYMORFISM
+
+class Employee:
+    def __init__(self, name, salary=30000):
+        self.name = name
+        self.salary = salary
+
+    def give_raise(self, amount):
+        self.salary += amount
+
+        
+class Manager(Employee):
+    def display(self):
+        print("Manager ", self.name)
+
+    def __init__(self, name, salary=50000, project=None):
+        Employee.__init__(self, name, salary)
+        self.project = project
+
+    # Add a give_raise method
+    def give_raise(self, amount, bonus=1.05):
+        Employee.give_raise(self, amount * bonus)
+        """
--- classes.py
+++ classes.py
@@ -1,42 +1,42 @@
+
+import pandas as pd
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+import time
+
+
 class Website:
-    def define(self, domain, webpage_filter):
+    CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
+
+    def __init__(self, domain, webpage_filter):
         self.domain = domain
         self.webpage_filter = webpage_filter
+        self.main_page_html = ""
+
+    def get_html(self):
+        driver = webdriver.Chrome(Website.CHROMEDRIVER_PATH)
+        driver.get(self.webpage_filter)
+        time.sleep(5)
+        html = driver.page_source
+        time.sleep(10)
+        soup = bs(html, "html.parser")
+        return soup
+
+    def get_ads_urls_set(self):
+        main_page_html = soup
+        all_offers_html = main_page_html.find_all(
+            "a", {"class": "offer__outer"})
+        offers_paths_set = set()
+        for i in all_offers_html:
+            link = i['href']
+            full_url = self.domain + link
+            offers_paths_set.add(full_url)
+        return offers_paths_set
 
 
-morizon = Website()
-morizon.define("https://www.morizon.pl", "https://www.morizon.pl/mieszkania/warszawa/?ps%5Bbuild_year_from%5D=1970&ps%5Bliving_area_from%5D=39&ps%5Bmarket_type%5D=2&ps%5Bnumber_of_rooms_from%5D=2&ps%5Bnumber_of_rooms_to%5D=3&ps%5Bprice_from%5D=380000&ps%5Bprice_to%5D=460000&ps%5Bwith_photo%5D=1")
+morizon = Website("https://www.morizon.pl", "https://www.morizon.pl/mieszkania/warszawa/?ps%5Bbuild_year_from%5D=1970&ps%5Bliving_area_from%5D=39&ps%5Bmarket_type%5D=2&ps%5Bnumber_of_rooms_from%5D=2&ps%5Bnumber_of_rooms_to%5D=3&ps%5Bprice_from%5D=380000&ps%5Bprice_to%5D=460000&ps%5Bwith_photo%5D=1")
 
-otodom = Website()
-otodom.define("https://www.otodom.pl", "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie,rynek-wtorny/warszawa?distanceRadius=0&page=1&limit=36&priceMax=450000&areaMin=39&ownerTypeSingleSelect=ALL&buildYearMin=1970&roomsNumber=%5BTWO%2CTHREE%5D&locations=%5Bcities_6-26%5D&by=DEFAULT&direction=DESC&viewType=listing")
+otodom = Website("https://www.otodom.pl", "https://www.otodom.pl/pl/oferty/sprzedaz/mieszkanie,rynek-wtorny/warszawa?distanceRadius=0&page=1&limit=36&priceMax=450000&areaMin=39&ownerTypeSingleSelect=ALL&buildYearMin=1970&roomsNumber=%5BTWO%2CTHREE%5D&locations=%5Bcities_6-26%5D&by=DEFAULT&direction=DESC&viewType=listing")
 
-nieruchomosci_pl = Website()
-nieruchomosci_pl.define("https://warszawa.nieruchomosci-online.pl",
-                        "https://warszawa.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,sprzedaz,,Warszawa:20571,,,,-450000,39,,,,,,2,,,,,,,,,,,,,,,,,,,,,,,1")
-
-
-"""
-class Player:
-    MAX_POSITION = 10
-    def __init__(self):
-        self.position = 0
-
-    # Add a move() method with steps parameter
-    def move(self, steps):
-        self.steps = steps
-        if self.position + self.steps < Player.MAX_POSITION:
-            self.position = self.position + self.steps
-        else:
-            self.position = Player.MAX_POSITION
-        return self.position
-
-    # This method provides a rudimentary visualization in the console
-    def draw(self):
-        drawing = "-" * self.position + "|" +"-"*(Player.MAX_POSITION - self.position)
-        print(drawing)
-
-p = Player(); p.draw()
-p.move(4); p.draw()
-p.move(5); p.draw()
-p.move(3); p.draw()
-"""
+nieruchomosci_pl = Website("https://warszawa.nieruchomosci-online.pl",
+                           "https://warszawa.nieruchomosci-online.pl/szukaj.html?3,mieszkanie,sprzedaz,,Warszawa:20571,,,,-450000,39,,,,,,2,,,,,,,,,,,,,,,,,,,,,,,1")
